4b56289ff4c957857c468f4b74f20060cbe631b5
Cristine Gacos <crstn.anne27@outlook.com>
2023-10-05 21:29:16 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lbl_name = New Label()
+        lbl_age = New Label()
+        txtb_name = New TextBox()
+        txtb_age = New TextBox()
+        lbl_occupation = New Label()
+        txtb_occupation = New TextBox()
+        btn_saveData = New Button()
+        SuspendLayout()
+        ' 
+        ' lbl_name
+        ' 
+        lbl_name.AutoSize = True
+        lbl_name.Location = New Point(30, 37)
+        lbl_name.Name = "lbl_name"
+        lbl_name.Size = New Size(42, 15)
+        lbl_name.TabIndex = 0
+        lbl_name.Text = "Name:"
+        ' 
+        ' lbl_age
+        ' 
+        lbl_age.AutoSize = True
+        lbl_age.Location = New Point(30, 75)
+        lbl_age.Name = "lbl_age"
+        lbl_age.Size = New Size(31, 15)
+        lbl_age.TabIndex = 1
+        lbl_age.Text = "Age:"
+        ' 
+        ' txtb_name
+        ' 
+        txtb_name.Location = New Point(120, 34)
+        txtb_name.Name = "txtb_name"
+        txtb_name.Size = New Size(183, 23)
+        txtb_name.TabIndex = 2
+        ' 
+        ' txtb_age
+        ' 
+        txtb_age.Location = New Point(120, 72)
+        txtb_age.Name = "txtb_age"
+        txtb_age.Size = New Size(100, 23)
+        txtb_age.TabIndex = 3
+        ' 
+        ' lbl_occupation
+        ' 
+        lbl_occupation.AutoSize = True
+        lbl_occupation.Location = New Point(30, 112)
+        lbl_occupation.Name = "lbl_occupation"
+        lbl_occupation.Size = New Size(69, 15)
+        lbl_occupation.TabIndex = 4
+        lbl_occupation.Text = "Occupation"
+        ' 
+        ' txtb_occupation
+        ' 
+        txtb_occupation.Location = New Point(120, 109)
+        txtb_occupation.Name = "txtb_occupation"
+        txtb_occupation.Size = New Size(183, 23)
+        txtb_occupation.TabIndex = 5
+        ' 
+        ' btn_saveData
+        ' 
+        btn_saveData.Location = New Point(30, 161)
+        btn_saveData.Name = "btn_saveData"
+        btn_saveData.Size = New Size(273, 23)
+        btn_saveData.TabIndex = 6
+        btn_saveData.Text = "Save data"
+        btn_saveData.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(346, 226)
+        Controls.Add(btn_saveData)
+        Controls.Add(txtb_occupation)
+        Controls.Add(lbl_occupation)
+        Controls.Add(txtb_age)
+        Controls.Add(txtb_name)
+        Controls.Add(lbl_age)
+        Controls.Add(lbl_name)
+        Name = "Form1"
+        Text = "2 Dimensional Array"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lbl_name As Label
+    Friend WithEvents lbl_age As Label
+    Friend WithEvents txtb_name As TextBox
+    Friend WithEvents txtb_age As TextBox
+    Friend WithEvents lbl_occupation As Label
+    Friend WithEvents txtb_occupation As TextBox
+    Friend WithEvents btn_saveData As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.Text
+
+Public Class Form1
+
+    Dim arr_profile(3, 0) As String
+
+    Dim int_row As Integer
+
+    Dim str_name As String
+    Dim str_age As String
+    Dim str_occupation As String
+
+    Dim int_age As Integer
+
+    Private Sub btn_saveData_Click(sender As Object, e As EventArgs) Handles btn_saveData.Click
+
+        str_name = txtb_name.Text
+        str_age = txtb_age.Text
+        str_occupation = txtb_occupation.Text
+
+        Dim bool_inputsCheck As Boolean
+
+        'Dim str_output As New StringBuilder()
+        Dim str_output As String
+
+        If String.IsNullOrEmpty(str_name) Or String.IsNullOrWhiteSpace(str_name) Or
+           String.IsNullOrEmpty(str_age) Or String.IsNullOrWhiteSpace(str_age) Or
+           String.IsNullOrEmpty(str_occupation) Or String.IsNullOrWhiteSpace(str_occupation) Then
+            MessageBox.Show("Please Complete the Form.")
+        ElseIf Not IsNumeric(str_age) Then
+            MessageBox.Show("Please Input a Valid Number")
+        ElseIf IsNumeric(str_age) Then
+            int_age = str_age
+            If int_age > 0 Then
+                bool_inputsCheck = True
+            Else
+                MessageBox.Show("Please Input a VALID AGE")
+            End If
+        Else
+            bool_inputsCheck = True
+        End If
+
+
+        If bool_inputsCheck = True Then
+
+            If int_row = 0 Then
+                int_row += 1
+                ReDim arr_profile(2, int_row)
+            Else
+                int_row += 1
+                ReDim Preserve arr_profile(2, int_row)
+            End If
+
+            arr_profile(0, int_row - 1) = str_name
+            arr_profile(1, int_row - 1) = str_age
+            arr_profile(2, int_row - 1) = str_occupation
+
+            txtb_name.Clear()
+            txtb_age.Clear()
+            txtb_occupation.Clear()
+
+            MessageBox.Show(int_row & " Data Saved! ")
+
+            For i = 0 To int_row - 1 Step 1
+
+                For j = 0 To 2 Step 1
+                    'str_output.Append(arr_profile(j, i) & vbNewLine)
+                    str_output = str_output & arr_profile(j, i) & vbNewLine
+                Next
+
+                'MessageBox.Show(str_output.ToString)
+                MessageBox.Show(str_output)
+                str_output = ""
+            Next
+        End If
+    End Sub
+End Class
